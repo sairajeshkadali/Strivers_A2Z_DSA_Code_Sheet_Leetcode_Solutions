228ce0e193cddb382aacda8be7f427a17042d84a
Kadali Sai Rajesh <117368222+sairajeshkadali@users.noreply.github.com>
2026-01-06 10:21:59 +0530

    Time: 3 ms (52.02%), Space: 26.3 MB (27.21%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map <int, int> hashMap;
+        int min = n / 3 + 1;
+        unordered_map<int, int> hash;
         vector<int> sol;
-        for (int i = 0; i < n; i++)
-            hashMap[nums[i]]++;
-        
-        for(auto it : hashMap){
-            if(it.second > n / 3)
-                sol.emplace_back(it.first);
+        for (int i = 0; i < n; i++) {
+            hash[nums[i]]++;
+            if (hash[nums[i]] == min)
+                sol.emplace_back(nums[i]);
         }
         return sol;
     }
