19b87d10813eae930490c1ab0f189454a61c6184
Kadali Sai Rajesh <117368222+sairajeshkadali@users.noreply.github.com>
2026-01-06 11:00:08 +0530

    Time: 0 ms (100%), Space: 23.6 MB (81.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,14 +2,38 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int min = n / 3 + 1;
-        unordered_map<int, int> hash;
+        int count1 = 0, count2 = 0, ele1 = INT_MIN, ele2 = INT_MIN;
         vector<int> sol;
         for (int i = 0; i < n; i++) {
-            hash[nums[i]]++;
-            if (hash[nums[i]] == min)
-                sol.emplace_back(nums[i]);
+            if (count1 == 0 && nums[i] != ele2) {
+                ele1 = nums[i];
+                count1++;
+            } else if (count2 == 0 && nums[i] != ele1) {
+                ele2 = nums[i];
+                count2++;
+            } else if (nums[i] == ele1) {
+                count1++;
+            } else if (nums[i] == ele2) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
         }
+        count1 = count2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (ele1 == nums[i])
+                count1++;
+            if (ele2 == nums[i])
+                count2++;
+        }
+        int min = n / 3 + 1;
+        if (count1 >= min)
+            sol.emplace_back(ele1);
+        if (count2 >= min)
+            sol.emplace_back(ele2);
+        if (sol.size() == 2 && sol[0] > sol[1])
+            swap(sol[0], sol[1]);
         return sol;
     }
 };
