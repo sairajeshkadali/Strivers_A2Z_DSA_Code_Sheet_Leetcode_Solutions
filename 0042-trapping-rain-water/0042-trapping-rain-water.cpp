875effdbb58835a068fd6241762759ab4366feb3
Kadali Sai Rajesh <117368222+sairajeshkadali@users.noreply.github.com>
2026-02-10 17:27:14 +0530

    Time: 0 ms (100%), Space: 26.9 MB (42.59%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int n = height.size(), waterCount = 0, minHeight;
-        vector<int> prevGre(n), nextGre(n);
-        prevGre[0] = nextGre[n-1] = -1;
-        for(int i = 1, j = n - 2; i < n; i++, j--){
-            prevGre[i] = max(prevGre[i-1], height[i-1]);
-            nextGre[j] = max(nextGre[j + 1], height[j + 1]);
+        int n = height.size(), waterCount = 0, maxHeight = -1;
+        vector<int> prevGre(n);
+        prevGre[0] = prevGre[n - 1] = -1;
+        for (int i = 1; i < n - 1; i++) {
+            prevGre[i] = max(prevGre[i - 1], height[i - 1]);
         }
-        for(int i = 1; i < n - 1; i++){
-            minHeight = min(prevGre[i], nextGre[i]);
-            if(height[i] < minHeight){
-                waterCount += minHeight - height[i];
+        maxHeight = height[n - 1];
+        for (int j = n - 2; j >= 1; j--) {
+            prevGre[j] = min(prevGre[j], maxHeight);
+            maxHeight = max(maxHeight, height[j]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (height[i] < prevGre[i]) {
+                waterCount += prevGre[i] - height[i];
             }
         }
         return waterCount;
