439cb18f85be69ed78f41433ea33c1499adf0c60
Kadali Sai Rajesh <117368222+sairajeshkadali@users.noreply.github.com>
2026-02-11 11:50:58 +0530

    Time: 0 ms (100%), Space: 9.4 MB (14.26%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        int top = 0, left = 0, right = n - 1, bottom = m - 1;
+        int minr = 0, maxc = n - 1, maxr = m - 1, minc = 0, count = 0,
+            total = m * n;
         vector<int> sol;
-        while (top <= bottom && left <= right) {
-            for (int i = left; i <= right; i++)
-                sol.emplace_back(matrix[top][i]);
-            top++;
-            for (int i = top; i <= bottom; i++)
-                sol.emplace_back(matrix[i][right]);
-            right--;
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--)
-                    sol.emplace_back(matrix[bottom][i]);
-                bottom--;
+        while (minr <= maxr && minc <= maxc) {
+            for (int i = minc; i <= maxc && count < total; i++) {
+                sol.emplace_back(matrix[minr][i]);
+                count++;
             }
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--)
-                    sol.emplace_back(matrix[i][left]);
-                left++;
+            ++minr;
+            for (int j = minr; j <= maxr && count < total; j++) {
+                sol.emplace_back(matrix[j][maxc]);
+                count++;
             }
+            --maxc;
+            for (int k = maxc; k >= minc && count < total; k--) {
+                sol.emplace_back(matrix[maxr][k]);
+                count++;
+            }
+            --maxr;
+            for (int l = maxr; l >= minr && count < total; l--) {
+                sol.emplace_back(matrix[l][minc]);
+                count++;
+            }
+            ++minc;
         }
         return sol;
     }
